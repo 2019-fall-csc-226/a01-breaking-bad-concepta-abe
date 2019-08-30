86488a61258eb1053a8a908e3f64732fb7b347a6
morenoa2 <morenoa2@berea.edu>
2019-08-29 23:53:06 -0400

    Made it si a friend can also get their sign. Added functions to make life easier.

--- a01_stubs.py
+++ a01_stubs.py
@@ -17,17 +17,44 @@
 ######################################################################
 # (Required) Task 1
 # TODO Ask user for their birth year
-def findSign(year):
+def findSign(year, passThisAlong):
     arrOfSigns = ["Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon"]
-    for i in range(2001, 2013):
-        if year == i:
-            print("You are a: " + arrOfSigns[i - 2001])
+    if passThisAlong == 1:
+        for i in range(2001, 2013):
+            if year == i:
+                print("You are a: " + arrOfSigns[i - 2001])
+    else:
+        for i in range(2001, 2013):
+            if year == i:
+                print("Your friend is a: " + arrOfSigns[i - 2001])
 
-birthYear = int(input("What year were you born in?"))
-if birthYear < 2001 or birthYear > 2012:
-    print("Sorry, you must be born between 2001 and 2012...")
-    quit()
-findSign(birthYear)
+def isYearInRange(year, passThisAlong):
+    if passThisAlong == 1:
+        if year < 2001 or year > 2012:
+            print("Sorry, you must be born between 2001 and 2012...")
+            print("Try again...")
+            askQuestion(1)
+        else:
+            findSign(year, 1)
+    else:
+        if year < 2001 or year > 2012:
+            print("Sorry, your friend must be born between 2001 and 2012...")
+            print("Try again...")
+            askQuestion(2)
+        else:
+            findSign(year, 2)
+
+def askQuestion(passThisAlong):
+    if passThisAlong == 1:
+        birthYear = int(input("What year were you born in?"))
+        isYearInRange(birthYear, passThisAlong)
+    else:
+        birthYear = int(input("What year was your friend born in?"))
+        isYearInRange(birthYear, 2)
+
+askQuestion(1)
+askQuestion(2)
+
 
 # TODO Check the year using if conditionals, and print the correct animal for that year.
 # See the a01_pets.py for examples
